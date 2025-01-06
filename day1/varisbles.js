af0d77e423a5ab393a0bd69f1420488e95cd59ff
akshayrachakonda04 <2211cs010487@mallareddyuniversity.ac.in>
2025-01-06 11:42:51 +0530

    day2

---
+++ day1/varisbles.js
@@ -0,0 +1,51 @@
+// console.log("Satrt")
+
+// var a = 10;
+// console.log(a)
+
+// let b = 20;
+// console.log(b)
+
+// const c = 30;
+// console.log(c)
+
+// {
+//     var a = 100;
+//     console.log(a)
+
+//     let b = 200;
+//     console.log(b)
+
+//     const c = 300
+//     console.log(c)
+// }
+
+// console.log("Stop")
+
+
+let userName = "Akshay";
+console.log(userName, typeof(userName));
+
+let str2 = 'single Quote';
+console.log(str2, typeof(str2))
+
+let str3 = `120cr`;
+console.log(str3, typeof(str3))
+
+let bool1 = true
+console.log(bool1, typeof(bool1))
+
+let bool2 = false
+console.log(bool2, typeof(bool2))
+
+let num1 = 10;
+console.log(num1, typeof(num1));
+
+let num2 = 10e3;
+console.log(num2, typeof(num2));
+
+let a;
+console.log(a);
+
+let b = null;
+console.log(b);
