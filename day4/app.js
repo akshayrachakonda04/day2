915b1ce08cde2c5636d2915bcf410f56b241a8b8
akshayrachakonda04 <2211cs010487@mallareddyuniversity.ac.in>
2025-01-08 13:17:42 +0530

    newfile

---
+++ day4/app.js
@@ -0,0 +1,19 @@
+// for (let i=1; i<=100; i++){
+//     if (i%2 == 0){
+//         console.log(i+" "+"Even number");
+//     }
+//     else{
+//         console.log(i+" "+"Odd number")
+//     }
+// }
+
+// let a = 5;
+// let b = 1;
+// for (let i = 1; i<= a; i++){
+//     b = b*i;
+//     console.log(b)
+// }
+
+// assignment 1:
+// write a java script program to find the factors of a given number
+// 12 - 2, 3, 4, 6
