27b1ba6f0d9f6d5a0f723ff6d8b18b5addbea910
akshayrachakonda <akshayrachakonda5@gmail.com>
2025-01-31 11:35:21 +0530

    ,.

---
+++ day16/app.js
@@ -0,0 +1,17 @@
+// Default Modules
+
+// let a = 10;
+// export default a;
+
+
+
+//Named module
+
+//  export default  x = 10;
+// const user = "Akshay";
+// const users = ["a", "b", "c", "d"];
+
+
+// export {
+//     x, user, users
+// };
